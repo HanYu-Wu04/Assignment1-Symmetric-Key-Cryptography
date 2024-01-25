90e34ead6882eb87a29aa54e5ebfbf8e73cf2fd2
eswenke <swenke.ethan.us@gmail.com>
2024-01-25 10:07:48 -0800

    fixed cbc decrypt, working

--- task1.py
+++ task1.py
@@ -5,18 +5,16 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import sys
 
+
 def generate_key():
     return get_random_bytes(16)
+
 
 def generate_iv():
     return get_random_bytes(16)
 
+
 def pkcs7_pad(data, block_size):
-    # given a block of plaintext,
-    # the amount of bytes needed to be padded is (16 - (len(block) % 16)
-    # the value of the added bytes will also be the number of bytes padded from the above formula
-    # since file read in rb, the data is in bytes. check the length with regard to this, and 
-    # concatenate based on the above rules and return the new block
     if len(data) != 16:
         padding_size = block_size - (len(data) % block_size)
         padding = bytes([padding_size] * padding_size)
@@ -24,21 +22,17 @@ def pkcs7_pad(data, block_size):
     else:
         return data
 
+
 def pkcs7_unpad(data):
-    # the last byte of the data is the number of bytes padded
-    # check the last byte, and remove that many bytes from the end of the data
     padding_size = data[-1]
+
     if padding_size > len(data):
         raise Exception("Invalid padding...")
+    
     return data[:-padding_size]
 
+
 def ecb_encrypt(plaintext, key):
-    # divide the content string into blocks of 128 bits
-    # check if block is full 128 bits, if not add padding()
-    # run the encrypt, add that block to a new string
-        # try encrypting the string itself, if not just create a new
-        # empty string to add the encrypted block to
-    #return the final string entirely encrypted
     cipher = AES.new(key, AES.MODE_ECB)
     ciphertext = b""
 
@@ -46,67 +40,72 @@ def ecb_encrypt(plaintext, key):
         block = plaintext[i : i+AES.block_size]
         encrypted_block = cipher.encrypt(pkcs7_pad(block, AES.block_size))
         ciphertext += encrypted_block
-        if len(encrypted_block) != 16:
-            print("INCORRECT PADDING")
-            print("len of encrypted block: " + str(len(encrypted_block)))
-            print("len of unencrypted block: " + str(len(block)))
-            break
 
     return ciphertext
+
 
 def ecb_decrypt(ciphertext, key):
     cipher = AES.new(key, AES.MODE_ECB)
     decrypted = pkcs7_unpad(cipher.decrypt(ciphertext))
+
     return decrypted
 
+
 def cbc_encrypt(plaintext, key, IV):
-    # create an IV in order to XOR with the initial block of plaintext
-    # encrypt that block with the key (after checking for padding)
-    # add that encryption to the result string
-    # use that encrypted block to XOR with the next block of plaintext, after padding
-    cipher = AES.new(key, AES.MODE_CBC, IV)
+    cipher = AES.new(key, AES.MODE_ECB)
     ciphertext = b""
     previous_block = IV
+
     for i in range(0, len(plaintext), AES.block_size):
         block = plaintext[i : i+AES.block_size]
         xor_block = bytes([b1 ^ b2 for b1, b2 in zip(block, previous_block)])
         encrypted_block = cipher.encrypt(pkcs7_pad(xor_block, AES.block_size))
         ciphertext += encrypted_block
         previous_block = encrypted_block
-        if len(encrypted_block) != 16:
-            print("INCORRECT PADDING")
-            print("len of encrypted block: " + str(len(encrypted_block)))
-            print("len of unencrypted block: " + str(len(block)))
-
-            break
+        
     return ciphertext
 
+
 def cbc_decrypt(ciphertext, key, IV):
-    cipher = AES.new(key, AES.MODE_CBC, IV)
-    decrypted = pkcs7_unpad(cipher.decrypt(ciphertext))
-    return decrypted
+    block_size = AES.block_size
+    cipher = AES.new(key, AES.MODE_ECB)
+    plaintext = b""
+    blocks = [ciphertext[i:i+block_size] for i in range(0, len(ciphertext), block_size)]
+    
+    for i in range(len(blocks)):
+        decrypted_block = cipher.decrypt(blocks[i])
+        if i == 0:
+            decrypted_block = bytes([decrypted_block[j] ^ IV[j] for j in range(block_size)])
+        else:
+            decrypted_block = bytes([decrypted_block[j] ^ blocks[i-1][j] for j in range(block_size)])
+
+        plaintext += decrypted_block
+    
+    return plaintext
+
 
 def read_bmp_file(file_name):
     with open(file_name, 'rb') as file_in:
         content = file_in.read()
+        
     return content
+
 
 def write_bmp_file(file_name, content):
     with open(file_name, 'wb') as file_out:
         file_out.write(content)
 
+
 def write_encrypted_file(file_name, content):
     with open(file_name, 'wb') as file_out:
         file_out.write(content)
+
 
 def task1(file_path):
     plaintext = read_bmp_file(file_path)
 
     key = generate_key()
     IV = generate_iv()
-
-    output_ecb_file = 'output_ecb.bmp'
-    output_cbc_file = 'output_cbc.bmp'
 
     ECB_ciphertext = ecb_encrypt(plaintext, key)
     CBC_ciphertext = cbc_encrypt(plaintext, key, IV)
@@ -119,6 +118,7 @@ def task1(file_path):
     decrypted_CBC = cbc_decrypt(CBC_ciphertext, key, IV)
     write_bmp_file("decrypted_cbc.bmp", decrypted_CBC)
 
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python task1.py <input_bmp>")
@@ -126,6 +126,7 @@ def main():
 
     input_bmp = sys.argv[1]
     task1(input_bmp)
+
 
 if __name__ == "__main__":
     main()

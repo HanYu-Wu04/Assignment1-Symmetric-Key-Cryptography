47347ef51092fb42c225baefe9f2f0474c4d6130
eswenke <swenke.ethan.us@gmail.com>
2024-01-25 10:59:54 -0800

    working on submit verify functions

--- task2.py
+++ task2.py
@@ -2,39 +2,63 @@
 # Ethan Swenke and HanYu Wu
 # CSC-321-03
 import sys
-from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from urllib.parse import quote
+from task1 import *
 
-def submit(str):
+
+def task2():
+    IV = generate_key()
+    key = generate_key()
+    user_input = input("Enter a string: ")
+    submit_str = submit(user_input, key, IV)
+
+    return print(verify(submit_str, key, IV))
+
+
+def submit(str, key, IV):
     # URL encode any ; or = characters that appear in the user provided string
         # in ASCII, ; is %3B and = is %3D 
     # pad this new string using PCKS#7
     # encrypt this string using our CBC implemenation
+    str = quote(str)
+    app_str = "userid=456;userdata=" + str + ";session-id=31337"
+    encrypted_str = cbc_encrypt(app_str.encode('utf-8'), key, IV)
+    print(encrypted_str)
 
-    res_str = "userid=456; userdata=" + str + ";session-id=31337"
-    return res_str
+    return encrypted_str
 
-def verify(str):
+
+def verify(str, key, IV):
     # decrypt the encrypted string
-    # parse the string for the patter ";admin=true;"
+    # parse the string for the pattern ";admin=true;"
     # return true / false based on whether that string is present
+    substr = ";admin=true;"
+    decrypted_str = cbc_decrypt(str, key, IV)
+    print(decrypted_str)
     
-    return
+    return substr in decrypted_str.decode('utf-8')
+
 
 def generate_key():
     return get_random_bytes(16)
 
+
 def pkcs7_pad(data, block_size):
-    padding_size = block_size - (len(data) % block_size)
-    padding = bytes([padding_size] * padding_size)
-    return data + padding
+    if len(data) != 16:
+        padding_size = block_size - (len(data) % block_size)
+        padding = bytes([padding_size] * padding_size)
+        return data + padding
+    else:
+        return data
+
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python task2.py <input_str>")
+    if len(sys.argv) != 1:
+        print("Usage: python task2.py")
         return
 
-    input_str = sys.argv[1]
+    task2()
     
 
 if __name__ == "__main__":
